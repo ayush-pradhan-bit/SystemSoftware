5fd438c608ca38aaaca8bd729d754c716508e965
ayush-pradhan-bit <ayushpradhan1@gmail.com>
2021-01-21 12:24:13 +0400

    input two integers

---
+++ Exercise2/Exercise2.c/math.c
@@ -0,0 +1,16 @@
+
+
+#include <stdio.h>
+
+int main() {    
+
+    int number1, number2;
+    
+    printf("Enter first integer: \n");
+    scanf("%d", &number1);
+    printf("Enter second integer: \n" );
+    scanf("%d", &number2);
+ 
+ 
+ 
+ }
