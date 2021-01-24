b72b99b1732e56a7b737a4c179de02ad528766f1
ayush-pradhan-bit <ayushpradhan1@gmail.com>
2021-01-25 01:24:33 +0400

    Exercise 2 3a

--- Exercise2/Exercise2.c/math.c
+++ Exercise2/Exercise2.c/math.c
@@ -12,4 +12,16 @@ int main() {
     scanf("%d", &number1); //reads first variable from user
     printf("Enter second integer: \n" ); 
     scanf("%d", &number2); //reads second variable from user
+    // IF condition is used to compare the two integers provided by the user
+    if (number1 == number2)
+    printf("both the integers are equal");
+    else
+    if (number1>number2)
+    printf("First integer is greater");
+    else 
+    printf("Second integer is greater");
+    
+    return 0;
 }
+
+
