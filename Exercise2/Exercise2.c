8c7773e9f6a5b6ee741576a4d54c8dbd06ebed71
ayush-pradhan-bit <ayushpradhan1@gmail.com>
2021-01-26 00:27:21 +0400

    Updated Exercise2 codes

--- Exercise2/Exercise2.c
+++ Exercise2/Exercise2.c
@@ -12,5 +12,16 @@ int main() {
     scanf("%d", &num1); //reads first variable from user
     printf("Enter second integer: \n" ); 
     scanf("%d", &num2); //reads second variable from user
+    
+  if (num1 == num2){
+  	printf("both are equal"); //Prints if both integer are same
+  	}
+  else
+  if (num1 > num2){
+  	printf("First integer is greater");// Prints if first input integer is greater
+  	}
+  else {
+  	printf("Second integer is greater");//Prints if second input is greater
+  }
     return 0;
 }
