6e906ede508ceffe6e0996e076b6fe093fe0416f
ayush-pradhan-bit <ayushpradhan1@gmail.com>
2021-01-25 13:48:47 +0400

    Adding exercise2

---
+++ Exercise2/Exercise2.c
@@ -0,0 +1,16 @@
+// Title - Entering Integer Values
+// Author - Ayush Pradhan
+// Descript - user enters two integers and multiple task are done
+
+#include <stdio.h>
+
+int main() {
+//These next lines of code first prompt the user to give two integer and then prints it out
+  int num1, num2; //thisisvariable
+
+    printf("Enter first integer: \n"); 
+    scanf("%d", &num1); //reads first variable from user
+    printf("Enter second integer: \n" ); 
+    scanf("%d", &num2); //reads second variable from user
+    return 0;
+}
