9b0698bbb50309d2879415d958b3650adf45179d
ayush-pradhan-bit <ayushpradhan1@gmail.com>
2021-01-31 12:35:33 +0400

    EXercise2 done

--- Exercise2/Exercise2.c
+++ Exercise2/Exercise2.c
@@ -1,4 +1,4 @@
-// Title - Entering Integer Values
+// Title - Entering Integer Values given by user
 // Author - Ayush Pradhan
 // Descript - user enters two integers and multiple task are done
 //          - Random number is generated and then checked if it is odd or even or 0
