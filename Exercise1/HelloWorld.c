a3053e6acd74e33a44457e7738ea758689102832
ayush-pradhan-bit <ayushpradhan1@gmail.com>
2021-02-02 00:10:30 +0400

    Exercise1 done

---
+++ Exercise1/HelloWorld.c
@@ -0,0 +1,10 @@
+// Title - first c programming language
+// Author - Ayush Pradhan
+// Description - Print output
+
+#include <stdio.h>
+int main() {
+	printf("Hello World!!\n"); //prints Hello World!!
+	printf("Good day \n"); //prints yo
+	return 0;
+}
